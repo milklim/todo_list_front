5c0829cd17c8a25689ed9a420be9b741bf0e8cbd
milklim <klimbox@gmail.com>
2017-12-18 14:01:05 +0200

    add layout, reducers, tasklists component; change react-router version 4.0.2=>3.0.5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-import './index.css';
+import './index.css'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+import {Router, Route, browserHistory} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducers from 'reducers'
+import Layout from 'containers/layout'
+import Tasklists from 'components/tasklists'
+
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(thunk)
+))
+
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path='/' component={Tasklists} />
+
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
