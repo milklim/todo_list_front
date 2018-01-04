e5e903b4cb64ac2aa5d191abd88bb45969f06aa5
milklim <klimbox@gmail.com>
2018-01-04 14:29:18 +0200

    stable version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import './index.css'
 import './styles.css'
 
 import React from 'react'
@@ -12,10 +11,10 @@ import {syncHistoryWithStore} from 'react-router-redux'
 
 import reducers from 'reducers'
 import Layout from 'containers/layout'
+import AuthenticatedWrapper from 'containers/authenticatedWrapper'
 import Tasklists from 'components/tasklists'
 import SignUp from 'components/signUp'
 import SignIn from 'components/signIn'
-import SignOut from 'components/signOut'
 
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk)
@@ -29,8 +28,9 @@ ReactDOM.render(
             <Route component={Layout}>
                 <Route path='/sign_up' component={SignUp}/>
                 <Route path='/sign_in' component={SignIn}/>
-                <Route path='/sign_out' component={SignOut}/>
-                <Route path='/' component={Tasklists} />
+                <Route component={AuthenticatedWrapper}>
+                    <Route path='/' component={Tasklists} />
+                </Route>
             </Route>
         </Router>
     </Provider>,
